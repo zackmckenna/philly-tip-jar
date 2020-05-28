6f855732d3e7b13f692386e07081b2314940d715
Zack McKenna <zackmckenna@gmail.com>
2020-05-27 20:57:56 -0400

    Updates

---
+++ precache-manifest.c6dab160d909ec46df6dbee23cb7fb6e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aec8264dec50cf6f16beaabd0a587d91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "50048dbfa9ddb29ad9ad",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "03c489d9b8abc1c73d42",
+    "url": "/static/js/2.935e64b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.935e64b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50048dbfa9ddb29ad9ad",
+    "url": "/static/js/main.dc09558e.chunk.js"
+  },
+  {
+    "revision": "e06c945bf5f2c44e54a8",
+    "url": "/static/js/runtime-main.36880821.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
