b9ddc2e2ef0c31c7135686e2e1fd2a61030fec3c
Zack McKenna <zackmckenna@gmail.com>
2020-05-27 20:58:59 -0400

    Updates

---
+++ precache-manifest.bd3c663e717bb5f31bfebb8713e79a48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a04b7f6806d0cfc29c9f21281b077a38",
+    "url": "/philly-tip-jar/index.html"
+  },
+  {
+    "revision": "ad9322b4ea359dcef1a1",
+    "url": "/philly-tip-jar/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "03c489d9b8abc1c73d42",
+    "url": "/philly-tip-jar/static/js/2.935e64b7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/philly-tip-jar/static/js/2.935e64b7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad9322b4ea359dcef1a1",
+    "url": "/philly-tip-jar/static/js/main.b8150f6a.chunk.js"
+  },
+  {
+    "revision": "f979680c3c8fd4f9f7f0",
+    "url": "/philly-tip-jar/static/js/runtime-main.0e1051b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/philly-tip-jar/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c6dab160d909ec46df6dbee23cb7fb6e.js"
+  "/philly-tip-jar/precache-manifest.bd3c663e717bb5f31bfebb8713e79a48.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/philly-tip-jar/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
